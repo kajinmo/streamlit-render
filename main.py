c58eb1d1f9d5442b457001b681161be37b754a95
Luis Mo <kajin.mo@hotmail.com>
2024-06-26 22:50:24 -0300

    ajustes

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 ﻿import streamlit as st
 import pandas as pd
 
-st.write("Mama eu :sunglasses:")
 
 list_jogos = ['Alemanha x Brasil', 'Canadá x Bolívia', 'Espanha x Austrália']
-number = st.number_input("Insert a number", value=None, placeholder="Type a number...")
 
-for jogo in list_jogos:
-    st.write(r"Jogo: {jogo}")
-    st.write("The current number is ", number)
+with st.form("my_form"):
+    st.write("Mama eu :sunglasses:")
+
+    for jogo in list_jogos:
+        st.write(r"Jogo: {}".format(jogo))
+        time1 = jogo.split(' x ')[0]
+        time2 = jogo.split(' x ')[1]
+        number1 = st.number_input(time1, value=None, placeholder="Defina o número de gols de {}".format(time1))
+        number2 = st.number_input(time2, value=None, placeholder="Defina o número de gols de {}".format(time1))
 
 
-st.form_submit_button(label="Submit")
+    st.form_submit_button(label="Submit")
+    #  streamlit run main.py [-- script args]
