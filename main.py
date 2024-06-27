e34457504d4f3ebe4727259f32ef2dc3180cc2b2
Luis Mo <kajin.mo@hotmail.com>
2024-06-26 22:20:30 -0300

    first commitDD

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 ﻿import streamlit as st
 import pandas as pd
 
-st.write("Hello world")
+st.write("Mama eu :sunglasses:")
+
+list_jogos = ['Alemanha x Brasil', 'Canadá x Bolívia', 'Espanha x Austrália']
+number = st.number_input("Insert a number", value=None, placeholder="Type a number...")
+
+for jogo in list_jogos:
+    st.write(r"Jogo: {jogo}")
+    st.write("The current number is ", number)
+
+
+st.form_submit_button(label="Submit")
